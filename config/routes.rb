78416e359ee8d8412f0b755718a8d4652128e966
shinyrachel <rachel.kory@gmail.com>
2016-06-08 15:43:09 -0400

    bower set up, angular working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+
+  namespace :api, defaults:{format: :json} do 
+    resources :storycards
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
