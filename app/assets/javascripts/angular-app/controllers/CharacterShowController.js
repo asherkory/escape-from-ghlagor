7298bb5e28f9af446820b759cc65b48e606d312e
shinyrachel <rachel.kory@gmail.com>
2016-06-13 12:17:14 -0400

    dynamic character editing added

--- app/assets/javascripts/angular-app/controllers/CharacterShowController.js
+++ app/assets/javascripts/angular-app/controllers/CharacterShowController.js
@@ -1,7 +1,24 @@
-function CharacterShowController (Character, $stateParams) {
+function CharacterShowController (Character, $stateParams, $window) {
   var ctrl = this;
 
+  ctrl.visibleName = false;
+  ctrl.visibleGender = false;
+
+  ctrl.showNameForm = function () {
+    ctrl.visibleName = !ctrl.visibleName; // hide or show name form
+  };
+
+  ctrl.showGenderForm = function () {
+    ctrl.visibleGender = !ctrl.visibleGender // hide or show gender form
+  }
+
   ctrl.character = Character.get({ id: $stateParams.id });
+
+  ctrl.editCharacter = function () {
+    ctrl.character.$update(function () {
+      $window.alert("Character saved!")
+    });
+  };
 }
 
 angular
--- app/assets/javascripts/angular-app/models/Character.js
+++ app/assets/javascripts/angular-app/models/Character.js
@@ -1,5 +1,7 @@
 function Character ($resource) {
-  var Character = $resource('http://localhost:3000/api/characters/:id.json', {id: '@id'});
+  var Character = $resource('http://localhost:3000/api/characters/:id.json', {id: '@id'}, {
+  update: { method: 'PUT' }
+});
 
   return Character;
 }
