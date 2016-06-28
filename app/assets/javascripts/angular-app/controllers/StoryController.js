b1e15e359036ad1c232c19e1c2e4587d9b3c4fc6
shinyrachel <rachel.kory@gmail.com>
2016-06-28 18:33:30 -0400

    cleaned up comments

--- app/assets/javascripts/angular-app/controllers/StoryController.js
+++ app/assets/javascripts/angular-app/controllers/StoryController.js
@@ -2,15 +2,7 @@ function StoryController ($scope, Storycard) {
   
   var ctrl = this;
 
-  ctrl.story = Storycard.get({ id: 1 });
-  
-  // ctrl.newItems = []; // only display items if they're not already in InventoryService's currentItems
-
-  // ctrl.getNewItems = function () {
-  //   return ctrl.newItems = InventoryService.selectNewItems(ctrl.story.storycard.items); 
-  // } 
-      // every time a storycard displays, check each item to see if it's already in currentItems
-      
+  ctrl.story = Storycard.get({ id: 1 }); 
 
   $scope.$on('changeStory', function (event, storyId) {
     ctrl.story = Storycard.get({ id: storyId });
